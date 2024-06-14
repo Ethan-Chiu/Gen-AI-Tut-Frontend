99682afaf9ea30594f675ef1ea373346443b75ff
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-06-14 09:21:03 +0800

    Vue project setup with tailwind and shadcn

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <header>
+    <div class="wrapper">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </header> -->
+
+  <RouterView/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
