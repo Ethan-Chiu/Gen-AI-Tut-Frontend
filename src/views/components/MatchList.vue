0f35749c02a21c61fe06b8071a57fda5a16fb0d9
Ethan-Chiu <ethanchiu0608@gmail.com>
2024-07-25 01:08:36 +0800

    Refresh match. Match router link

--- src/views/components/MatchList.vue
+++ src/views/components/MatchList.vue
@@ -1,88 +1,97 @@
 <template>
   <div>
-    <button @click="fetchItems" :disabled="loading">Fetch Items</button>
     <p v-if="loading">Loading...</p>
     <p v-if="error">{{ error }}</p>
-    <ul v-if="!loading && !error">
+    <div v-if="!loading && !error">
       <Tabs default-value="all">
-        <TabsList class="w-full flex">
-          <TabsTrigger value="all" class="flex-1"> All </TabsTrigger>
-          <TabsTrigger value="graded" class="flex-1"> Graded </TabsTrigger>
-        </TabsList>
+        <div class="flex w-full">
+          <TabsList class="w-full flex h-10">
+            <TabsTrigger value="all" class="flex-1"> All </TabsTrigger>
+            <TabsTrigger value="graded" class="flex-1"> Graded </TabsTrigger>
+          </TabsList>
+          <Button class="ml-2 w-14" size="icon" @click="fetchItems">
+            <RefreshCcw class="size-4" />
+            <span class="sr-only">Refresh</span>
+          </Button>
+        </div>
         <TabsContent value="all" class="h-full w-full">
-          <li v-for="item of items" :key="item.id" class="my-2">
-            <div class="rounded-md border-2 p-3 hover:bg-muted">
-              <!-- Match Name & Date-->
-              <div class="flex items-center">
-                <div class="flex items-center gap-2">
-                  <Badge class="font-semibold">
-                    {{ item.name }}
-                  </Badge>
-                  <span
-                    :class="
-                      cn(
-                        'flex h-2 w-2 rounded-full',
-                        item.matchStatus === 'CREATED' ? 'bg-blue-600' : 'bg-green-600'
-                      )
-                    "
-                  />
-                  <div
-                    :class="
-                      cn(
-                        'text-xs font-medium',
-                        item.matchStatus === 'CREATED' ? 'text-blue-600' : 'text-green-600'
-                      )
-                    "
-                  >
-                    {{ item.matchStatus }}
+          <div v-for="item of items" :key="item.id" class="my-2">
+            <RouterLink :to="`/match/${item.id}`">
+              <div class="rounded-md border-2 p-3 hover:bg-muted">
+                <!-- Match Name & Date-->
+                <div class="flex items-center">
+                  <div class="flex items-center gap-2">
+                    <Badge class="font-semibold">
+                      {{ item.name }}
+                    </Badge>
+                    <span
+                      :class="
+                        cn(
+                          'flex h-2 w-2 rounded-full',
+                          item.matchStatus === 'CREATED' ? 'bg-blue-600' : 'bg-green-600'
+                        )
+                      "
+                    />
+                    <div
+                      :class="
+                        cn(
+                          'text-xs font-medium',
+                          item.matchStatus === 'CREATED' ? 'text-blue-600' : 'text-green-600'
+                        )
+                      "
+                    >
+                      {{ item.matchStatus }}
+                    </div>
+                  </div>
+                  <!-- Date -->
+                  <div class="ml-auto text-xs text-muted-foreground">
+                    {{
+                      formatDistanceToNow(new Date(item.players[0].createAt), { addSuffix: true })
+                    }}
                   </div>
                 </div>
-                <!-- Date -->
-                <div class="ml-auto text-xs text-muted-foreground">
-                  {{ formatDistanceToNow(new Date(item.players[0].createAt), { addSuffix: true }) }}
+                <!-- Topic -->
+                <div class="text-xs font-medium m-2">
+                  {{ item.topic.description }}
                 </div>
-              </div>
-              <!-- Topic -->
-              <div class="text-xs font-medium m-2">
-                {{ item.topic.description }}
-              </div>
 
-              <div class="flex flex-row">
-                <!-- Player 1 -->
-                <div class="flex-1">
-                  <div class="h-8 mb-4 rounded-full align-middle bg-muted flex items-center">
-                    <p class="text-center w-full">
-                      {{
-                        item.players.find((player) => player.playerId === item.firstPlayerId)
-                          ?.player.name
-                      }}
-                    </p>
+                <div class="flex flex-row">
+                  <!-- Player 1 -->
+                  <div class="flex-1">
+                    <div class="h-8 mb-4 rounded-full align-middle bg-muted flex items-center">
+                      <p class="text-center w-full">
+                        {{
+                          item.players.find((player) => player.playerId === item.firstPlayerId)
+                            ?.player.name
+                        }}
+                      </p>
+                    </div>
+                    <div v-if="item.result" class="text-4xl font-bold font-mono text-center">
+                      {{ item.result.points.find((p) => p.userId === item.firstPlayerId)?.point }}
+                    </div>
                   </div>
-                  <div v-if="item.result" class="text-4xl font-bold font-mono text-center">
-                    {{ item.result.points.find((p) => p.userId === item.firstPlayerId)?.point }}
-                  </div>
-                </div>
-                <Swords class="h-24 w-24 rounded-full p-6 bg-red-400" />
-                <!-- Player 2 -->
-                <div class="flex-1">
-                  <div class="h-8 mb-4 rounded-full align-middle bg-muted flex items-center">
-                    <p class="text-center w-full">
-                      {{
-                        item.players.find((player) => player.playerId !== item.firstPlayerId)
-                          ?.player.name
-                      }}
-                    </p>
-                  </div>
-                  <div v-if="item.result" class="text-4xl font-bold font-mono text-center">
-                    {{ item.result.points.find((p) => p.userId !== item.firstPlayerId)?.point }}
+                  <Swords class="h-24 w-24 rounded-full p-6 bg-red-400" />
+                  <!-- Player 2 -->
+                  <div class="flex-1">
+                    <div class="h-8 mb-4 rounded-full align-middle bg-muted flex items-center">
+                      <p class="text-center w-full">
+                        {{
+                          item.players.find((player) => player.playerId !== item.firstPlayerId)
+                            ?.player.name
+                        }}
+                      </p>
+                    </div>
+                    <div v-if="item.result" class="text-4xl font-bold font-mono text-center">
+                      {{ item.result.points.find((p) => p.userId !== item.firstPlayerId)?.point }}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </li>
+            </RouterLink>
+          </div>
         </TabsContent>
       </Tabs>
-    </ul>
+    </div>
   </div>
 </template>
 
@@ -90,7 +99,7 @@
 import { useMatchListStore } from '@/stores/matchList'
 import { storeToRefs } from 'pinia'
 import { onMounted } from 'vue'
-import { Swords } from 'lucide-vue-next'
+import { Swords, RefreshCcw } from 'lucide-vue-next'
 import { Separator } from '@/components/ui/separator'
 import { Button } from '@/components/ui/button'
 import { formatDistanceToNow } from 'date-fns'
